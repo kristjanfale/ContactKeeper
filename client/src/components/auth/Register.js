9dedb05075d118326dec6a176d6750628b97ebcf
Kristjan Fale <kristjan.fale@gmail.com>
2019-08-18 17:21:57 +0200

    Login & Register Forms

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '' // password comformation
+  });
+
+  const { name, email, password, password2 } = user;
+
+  // Set user object to whatever we type in inputs
+  const onChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('Register Submit');
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span style={{ color: '#ff9900' }}>Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' value={name} onChange={onChange} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
+          <input type='email' name='email' value={email} onChange={onChange} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password2'>Confirm Password</label>
+          <input
+            type='password'
+            name='password2'
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <input type='submit' value='Register' className='btn btn-primary' />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
